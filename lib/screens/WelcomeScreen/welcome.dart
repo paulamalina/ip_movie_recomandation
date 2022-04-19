b05e9d008b38274ba9ba60e7db4712d81a37246e
LucianB6 <lucianbaciu2@gmail.com>
2022-04-19 15:26:26 +0300

    update

--- lib/screens/WelcomeScreen/welcome.dart
+++ lib/screens/WelcomeScreen/welcome.dart
@@ -1,114 +1,82 @@
-<<<<<<< Updated upstream
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+ 
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    backgroundColor: Colors(34A0A4),
+    body: Column(   
+      child: Column(
         children: [
-          Center(child: Text('Let\'s find a movie',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-            ),),),
-          Padding(
+          child: Container(
+            width: 1440,
+            height: 58,
+            decoration: BoxDecoration(
+                 color: Colors(76C893),
+              )
+          
+          Row(
+            crossAxisAlignemt: CrossAxisAlignement.start,
+            children: [
+              Image.asset('assets/images/logo.png'),
+            ],
+          ),
+        ),
+
+          child: Container(
+            decoration: const BoxDecoration(
+              width: 347,
+              height: 58,
+              color: Colors(1A759F),
+              borderRadius: const BorderRadius.all(Radius.circular(40)),
+              
+            ),
+            child: Text(('Let\'s find a movie', style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+        Padding(
             padding: const EdgeInsets.all(80),
             child: Container(
               decoration: const BoxDecoration(
-                color: Colors.cyan,
+                color: Colors(76C893),
                 borderRadius: const BorderRadius.all(Radius.circular(40)),
               ),
               child: Column(
                 children: [
-                  Center(child: SvgPicture.asset('images/welcome.svg')),
+                  Center(child: SvgPicture.asset('images/Welcome2.svg')),
                 ],
               ),
             ),
           ),
-          Center(
-            child: Container(
-              width: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50.0),
-                color: Colors.greenAccent,
-              ),
-              child: TextButton(
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          
+          height: 58,
+            
+          decoration: const BoxDecoration(
+              color: Colors(1A759F),
+              borderRadius: const BorderRadius.all(Radius.circular(40)),            
+            ),
+          child: TextButton(
                 onPressed: () {
                   Navigator.pushNamed(context, "/choose");
                 } ,
                 child: Text('Get Started', style: TextStyle(
-                  color: Colors.black,
+                  color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
                 ),
               ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+        )
+        ]
+      )
+    )
+  )
 }
-=======
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/svg.dart';
-// class WelcomeScreen extends StatelessWidget {
-//   const WelcomeScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.teal,
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Center(child: Text('Let\'s find a movie',
-//             style: TextStyle(
-//               fontSize: 30,
-//               color: Colors.white,
-//             ),),),
-//           Padding(
-//             padding: const EdgeInsets.all(40),
-//             child: Container(
-//               width: double.infinity,
-//               decoration: const BoxDecoration(
-//                 color: Colors.cyan,
-//                 borderRadius: const BorderRadius.all(Radius.circular(40)),
-//               ),
-//               child: Column(
-//                 children: [
-//                   Center(child: SvgPicture.asset('assets/images/welcome.svg')),
-//                 ],
-//               ),
-//             ),
-//           ),
-//           Center(
-//             child: Container(
-//               width: 100,
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(50.0),
-//                 color: Colors.greenAccent,
-//               ),
-//               child: TextButton(
-//                 onPressed: () {
-//                   Navigator.pushNamed(context, "/choose");
-//                 } ,
-//                 child: Text('Get Started', style: TextStyle(
-//                   color: Colors.black,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
->>>>>>> Stashed changes
