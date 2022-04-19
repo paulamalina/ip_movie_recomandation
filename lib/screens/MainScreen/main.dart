4298b106a9d4755a0a2bdb062ace5acc0bb5a095
RotaruSebastian <79158854+RotaruSebastian@users.noreply.github.com>
2022-04-19 15:09:18 +0300

    Add files via upload

--- lib/screens/MainScreen/main.dart
+++ lib/screens/MainScreen/main.dart
@@ -2,7 +2,6 @@ import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
 import 'package:ip_movie_recomandation/widgets/Toggle.dart';
 
 class MainScreen extends StatelessWidget {
