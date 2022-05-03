9c723191aeb1ea10c81d047e44babbf9dc53a296
LucianB6 <lucianbaciu2@gmail.com>
2022-05-03 15:11:20 +0300

    gata

--- lib/screens/MainScreen/main.dart
+++ lib/screens/MainScreen/main.dart
@@ -1,24 +1,24 @@
-// import 'package:flutter/material.dart';
-//
-// class MainScreen extends StatelessWidget {
-//   const MainScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Main Screen'),
-//       ),
-//       body: Center(
-//         child: ElevatedButton(
-//           // Within the `WelcomeScreen` widget
-//           onPressed: () {
-//             // Navigate to the second screen using a named route.
-//             Navigator.pop(context);
-//           },
-//           child: const Text('go back'),
-//         ),
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          // Within the `WelcomeScreen` widget
+          onPressed: () {
+            // Navigate to the second screen using a named route.
+            Navigator.pop(context);
+          },
+          child: const Text('go back'),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/WelcomeScreen/welcome.dart
+++ lib/screens/WelcomeScreen/welcome.dart
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 class WelcomeScreen extends StatelessWidget {
@@ -6,109 +5,91 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text('Let\'s find a movie',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-            ),),),
-          Padding(
-            padding: const EdgeInsets.all(80),
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.cyan,
-                borderRadius: const BorderRadius.all(Radius.circular(40)),
-              ),
-              child: Column(
-                children: [
-                  Center(child: SvgPicture.asset('images/welcome.svg')),
-                ],
-              ),
-            ),
-          ),
-          Center(
-            child: Container(
-              width: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50.0),
-                color: Colors.greenAccent,
-              ),
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/choose");
-                } ,
-                child: Text('Get Started', style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                ),
-              ),
-            ),
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 7,
+          )
         ],
+        gradient: LinearGradient(colors: [Colors.white, Color.fromRGBO(52, 160, 164, 1)], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+           backgroundColor: Color.fromRGBO(26, 117, 159, 1),   
+           title: Container(
+             width: 60,
+             child: Image.asset(
+               "assets/images/logo.png",
+             ),
+           ),
+
+        ),
+        body: Center(
+          child: Container(
+            width: 600,
+            height: 600,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 7,
+                  spreadRadius: 2,
+                )
+              ],
+              borderRadius: BorderRadius.circular(50.0),
+              color: Colors.teal,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 400,
+                  height: 50,
+                  decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(50.0),
+                   color: Color.fromRGBO(26, 117, 159, 1),
+                    ),
+                  child: Center(
+                    child: Text('Let\'s find a movie', style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      
+                    ),),
+                  ),
+                ),
+                Center(
+                  child: SvgPicture.asset(
+                    "assets/images/welcome.svg",
+                  ),
+                ),
+                  SizedBox(
+                    width: 200,
+                    height: 50,
+                    child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(26, 117, 159, 1)),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                            ),
+                        )
+                        
+                      ),
+                      onPressed: (){
+                      Navigator.pushNamed(context, "/choose");
+                      },
+                      child: Text('Get started', style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-=======
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/svg.dart';
-// class WelcomeScreen extends StatelessWidget {
-//   const WelcomeScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.teal,
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Center(child: Text('Let\'s find a movie',
-//             style: TextStyle(
-//               fontSize: 30,
-//               color: Colors.white,
-//             ),),),
-//           Padding(
-//             padding: const EdgeInsets.all(40),
-//             child: Container(
-//               width: double.infinity,
-//               decoration: const BoxDecoration(
-//                 color: Colors.cyan,
-//                 borderRadius: const BorderRadius.all(Radius.circular(40)),
-//               ),
-//               child: Column(
-//                 children: [
-//                   Center(child: SvgPicture.asset('assets/images/welcome.svg')),
-//                 ],
-//               ),
-//             ),
-//           ),
-//           Center(
-//             child: Container(
-//               width: 100,
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(50.0),
-//                 color: Colors.greenAccent,
-//               ),
-//               child: TextButton(
-//                 onPressed: () {
-//                   Navigator.pushNamed(context, "/choose");
-//                 } ,
-//                 child: Text('Get Started', style: TextStyle(
-//                   color: Colors.black,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
->>>>>>> Stashed changes
